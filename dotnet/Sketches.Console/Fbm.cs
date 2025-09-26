b587235b184e245b6624ffd97c4c92b0311d7611
Nathan Landis <nlandis@parraid.com>
2025-09-26 13:59:53 -0400

    get noise compiling

--- dotnet/Sketches.Console/Fbm.cs
+++ dotnet/Sketches.Console/Fbm.cs
@@ -11,16 +11,17 @@ namespace Sketches.Console;
 /// </source>
 public class Fbm
 {
-    double fbm(vecN x, in double H)
+    float fbm(Vector3 x, float H)
     {
-        double G = exp2(-H);
-        double f = 1.0;
-        double a = 1.0;
-        double t = 0.0;
+        var numOctaves = 3;
+        float G = (float)Math.Pow(2f, -H);
+        float f = 1.0f;
+        float a = 1.0f;
+        float t = 0.0f;
         for (int i = 0; i < numOctaves; i++)
         {
-            t += a * noise(f * x);
-            f *= 2.0;
+            t += a * noise(Vector3.Multiply(f, x));
+            f *= 2.0f;
             a *= G;
         }
         return t;
@@ -61,34 +62,34 @@ public class Fbm
         return -1.0f + 2.0f * (k0 + k1 * u[0] + k2 * u[1] + k3 * u[2] + k4 * u[0] * u[1] + k5 * u[1] * u[2] + k6 * u[2] * u[0] + k7 * u[0] * u[1] * u[2]);
     }
 
-    double ValueNoise(Vector3 x)
-    {
-        var p = new Vector3(float.Floor(x[0]), float.Floor(x[1]), float.Floor(x[2]));
-        var w = x - p;
+    // double ValueNoise(Vector3 x)
+    // {
+    //     var p = new Vector3(float.Floor(x[0]), float.Floor(x[1]), float.Floor(x[2]));
+    //     var w = x - p;
 
-        var u = w * w * w * (w * (Vector3.Multiply(6, w) - fifteen) + ten);
-        var du = Vector3.Multiply(30.0f, w) * w * (w * (w - one) + two);
+    //     var u = w * w * w * (w * (Vector3.Multiply(6, w) - fifteen) + ten);
+    //     var du = Vector3.Multiply(30.0f, w) * w * (w * (w - one) + two);
 
-        float a = hash1(p + new Vector3(0, 0, 0));
-        float b = hash1(p + new Vector3(1, 0, 0));
-        float c = hash1(p + new Vector3(0, 1, 0));
-        float d = hash1(p + new Vector3(1, 1, 0));
-        float e = hash1(p + new Vector3(0, 0, 1));
-        float f = hash1(p + new Vector3(1, 0, 1));
-        float g = hash1(p + new Vector3(0, 1, 1));
-        float h = hash1(p + new Vector3(1, 1, 1));
+    //     float a = hash1(p + new Vector3(0, 0, 0));
+    //     float b = hash1(p + new Vector3(1, 0, 0));
+    //     float c = hash1(p + new Vector3(0, 1, 0));
+    //     float d = hash1(p + new Vector3(1, 1, 0));
+    //     float e = hash1(p + new Vector3(0, 0, 1));
+    //     float f = hash1(p + new Vector3(1, 0, 1));
+    //     float g = hash1(p + new Vector3(0, 1, 1));
+    //     float h = hash1(p + new Vector3(1, 1, 1));
 
-        float k0 = a;
-        float k1 = b - a;
-        float k2 = c - a;
-        float k3 = e - a;
-        float k4 = a - b - c + d;
-        float k5 = a - c - e + g;
-        float k6 = a - b - e + f;
-        float k7 = -a + b + c - d + e - f - g + h;
+    //     float k0 = a;
+    //     float k1 = b - a;
+    //     float k2 = c - a;
+    //     float k3 = e - a;
+    //     float k4 = a - b - c + d;
+    //     float k5 = a - c - e + g;
+    //     float k6 = a - b - e + f;
+    //     float k7 = -a + b + c - d + e - f - g + h;
 
-        return -1.0 + 2.0 * (k0 + k1 * u.x + k2 * u.y + k3 * u.z + k4 * u.x * u.y + k5 * u.y * u.z + k6 * u.z * u.x + k7 * u.x * u.y * u.z;
-    }
+    //     return -1.0 + 2.0 * (k0 + k1 * u.x + k2 * u.y + k3 * u.z + k4 * u.x * u.y + k5 * u.y * u.z + k6 * u.z * u.x + k7 * u.x * u.y * u.z;
+    // }
 
     float hash1(Vector2 p)
     {
